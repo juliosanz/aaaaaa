b2a8c8fe1babd0774abafe80e1d5e58e99064e6c
Mega646 <jsanz.r64@gmail.com>
2021-04-03 16:43:43 +0200

    Menu implementado

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    public Button botonTryAgain;
+    public Button botonMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonTryAgain.onClick.AddListener(delegate { TaskOnClick(1); });
+        botonMenu.onClick.AddListener(delegate { TaskOnClick(2); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick(int i)
+    {
+        switch(i)
+        {
+            case 1:
+                SceneManager.LoadScene("minijuego");
+                break;
+            case 2:
+                Debug.Log("Menu pulsado");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/ControladorMeta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorMeta : MonoBehaviour
+{
+    public GameObject bola1;
+    public GameObject bola2;
+    public GameObject texto;
+    public GameObject tryAgainButton;
+    private MovimientoBolas bola1script;
+    private MovimientoBolas bola2script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bola1script = bola1.GetComponent<MovimientoBolas>();
+        bola2script = bola2.GetComponent<MovimientoBolas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bola1script.enabled && !bola2script.enabled)
+        {
+            texto.SetActive(true);
+            tryAgainButton.SetActive(true);
+        }
+    }
+}
