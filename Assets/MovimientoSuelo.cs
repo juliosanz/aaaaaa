19251d234c60d38eb43538be2185adc495309cf1
Mega646 <jsanz.r64@gmail.com>
2021-04-03 13:03:48 +0200

    Laberinto diseñado

---
+++ Assets/MovimientoSuelo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSuelo : MonoBehaviour
+{
+    public float velocidad = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.deviceOrientation.Equals(DeviceOrientation.FaceUp))
+        {
+            Vector3 movimiento = new Vector3(Input.acceleration.x, 0f, Input.acceleration.y);
+            transform.Translate(movimiento * velocidad * Time.deltaTime);
+        }
+    }
+}
