06a32f4642d40e68601a6ce5a83f9ad29d183fbb
Mega646 <jsanz.r64@gmail.com>
2021-04-04 11:59:20 +0200

    bola1script y bola2script renombrados

--- Assets/Scripts/ControladorMeta.cs
+++ Assets/Scripts/ControladorMeta.cs
@@ -8,19 +8,19 @@ public class ControladorMeta : MonoBehaviour
     public GameObject bola2;
     public GameObject texto;
     public GameObject tryAgainButton;
-    private MovimientoBolas bola1script;
-    private MovimientoBolas bola2script;
+    private MovimientoBolas bola1movimiento;
+    private MovimientoBolas bola2movimiento;
     // Start is called before the first frame update
     void Start()
     {
-        bola1script = bola1.GetComponent<MovimientoBolas>();
-        bola2script = bola2.GetComponent<MovimientoBolas>();
+        bola1movimiento = bola1.GetComponent<MovimientoBolas>();
+        bola2movimiento = bola2.GetComponent<MovimientoBolas>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!bola1script.enabled && !bola2script.enabled)
+        if(!bola1movimiento.enabled && !bola2movimiento.enabled)
         {
             texto.SetActive(true);
             tryAgainButton.SetActive(true);
