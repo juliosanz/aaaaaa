80d66769a661a5de07fb504809f05ab36a4ddfee
GuillermoFernandezM <guillermo.fernandezm@alumnos.upm.es>
2021-04-16 16:48:27 +0200

    Gira il mondo Gira

---
+++ Assets/Scripts/Geolocalización/CompasMundo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompasMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.location.Start();
+        Input.compass.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, -Input.compass.trueHeading, 0);
+    }
+}
